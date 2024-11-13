b287efe3a99fea3e63c47e58a86082e4634932b7
Romain Dénarié <romain.denarie@exoplatform.com>
2024-11-13 15:22:31 +0100

    Add 2 tabs in the drawer

--- webapps/src/main/webapp/vue-apps/ixbus-top-bar/components/IxbusDrawer.vue
+++ webapps/src/main/webapp/vue-apps/ixbus-top-bar/components/IxbusDrawer.vue
@@ -8,12 +8,52 @@
       {{ $t('IxbusPortlet.drawer.title') }}
     </template>
     <template slot="content">
-
+      <div class="d-flex overflow-hidden full-width">
+        <v-tabs
+          class="flex-grow-1 flex-shrink-1">
+          <v-tab
+            tab-value="actions"
+            v-show="this.actionsCount > 0"
+            href="#actions">
+            {{ $t('ixbus.drawer.tab.actions') }}
+            <v-avatar
+              v-show="this.actionsCount > 0"
+              color="secondary"
+              min-height="16"
+              min-width="16"
+              height="auto"
+              width="auto"
+              class="ms-2 pa-1 aspect-ratio-1 white--text content-box-sizing">
+              {{ this.actionsCount }}
+            </v-avatar>
+          </v-tab>
+          <v-tab
+            tab-value="myFolders"
+            v-show="this.foldersCount > 0"
+            href="#myFolders">
+            {{ $t('ixbus.drawer.tab.myfolders') }}
+            <v-avatar
+              v-show="this.foldersCount > 0"
+              color="secondary"
+              min-height="16"
+              min-width="16"
+              height="auto"
+              width="auto"
+              class="ms-2 pa-1 aspect-ratio-1 white--text content-box-sizing">
+              {{ this.foldersCount }}
+            </v-avatar>
+          </v-tab>
+        </v-tabs>
+      </div>
     </template>
   </exo-drawer>
 </template>
 <script>
 export default {
+  data: () => ({
+    actionsCount: 0,
+    foldersCount: 0
+  }),
   created() {
     this.$root.$on('open-ixbus-drawer', () => {
       this.openDrawer();
@@ -21,6 +61,10 @@ export default {
   },
   methods: {
     openDrawer() {
+      this.$ixbusService.getCurrentUserFoldersCount()
+        .then((data) => {
+          this.actionsCount = data?.count || 0;
+        });
       this.$refs.ixbusDrawer.open();
     },
   }
